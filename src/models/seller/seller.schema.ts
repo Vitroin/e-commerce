9d97729e40bac82ea0dc6ce06de41801c5ef2ff7
Mohamed Elsayed <Mohamed.elsayedt10@gmail.com>
2026-07-19 03:42:41 +0300

    refactor: extend Seller class from User schema

--- src/models/seller/seller.schema.ts
+++ src/models/seller/seller.schema.ts
@@ -1,10 +1,9 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { User } from "../common/user.schema";
 
 @Schema({ timestamps: true, discriminatorKey:"role" ,toJSON: {virtuals: true} })
-export class Seller {
-    userName!: string;   
-    email!: string;
-    password!: string 
+export class Seller extends User {
+
 
     @Prop({type: String, required: true})
     whatsappLink!: string;
