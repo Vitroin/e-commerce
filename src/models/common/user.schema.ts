5ea425798086707dbeb74d103d1196b8f7f6fcf1
Mohamed Elsayed <Mohamed.elsayedt10@gmail.com>
2026-07-19 03:40:20 +0300

    refactor: use definite assignment assertions for user and seller properties

--- src/models/common/user.schema.ts
+++ src/models/common/user.schema.ts
@@ -3,13 +3,13 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 @Schema({ timestamps: true, discriminatorKey:"role" ,toJSON: {virtuals: true} })
 export class User {
     @Prop({type: String, required: true})
-    userName: string;
+    userName!: string;
     
     @Prop({type: String, required: true, unique: true})
-    email: string;
+    email!: string;
 
     @Prop({type: String, required: true})
-    password: string 
+    password!: string 
 }
 
 export const userSchema = SchemaFactory.createForClass(User);
--- src/models/seller/seller.schema.ts
+++ src/models/seller/seller.schema.ts
@@ -2,12 +2,12 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
 @Schema({ timestamps: true, discriminatorKey:"role" ,toJSON: {virtuals: true} })
 export class Seller {
-    userName: string;   
-    email: string;
-    password: string 
+    userName!: string;   
+    email!: string;
+    password!: string 
 
     @Prop({type: String, required: true})
-    whatsappLink: string;
+    whatsappLink!: string;
 }
 
 export const sellerSchema =  SchemaFactory.createForClass(Seller);
